f1cde530de02f323bb686c1d6f8aa87c173c4cc8
Hunkizzz <hunkizzzx777@gmail.com>
2023-06-27 03:23:29 +0300

    init. First tests

--- requests_file.py
+++ requests_file.py
@@ -1,5 +1,8 @@
 import requests
 from typing import Dict
+from typing import Union
+from decimal import Decimal
+from datetime import datetime
 
 async def get_crypto_currency(name: str) -> Dict[str, str]:
     url = f"http://localhost:8095/coinmarketcap/api/crypto/{name}"
@@ -9,9 +12,42 @@ async def get_crypto_currency(name: str) -> Dict[str, str]:
         return data
     else:
         raise Exception("Error occurred while fetching data from the API")
+async def format_value_data(dto: Dict[str, str]) -> str:
+    formatted_str = f"Symbol: {dto['symbol']}\n"
+    formatted_str += f"Price: {format_decimal(dto['price'])}\n"
+    formatted_str += f"24h Volume: {format_decimal(dto['volume_24h'])}\n"
+    formatted_str += f"24h Volume Change: {format_decimal(dto['volume_change_24h'])}%\n"
+    formatted_str += f"1h Percent Change: {format_decimal(dto['percent_change_1h'])}%\n"
+    formatted_str += f"24h Percent Change: {format_decimal(dto['percent_change_24h'])}%\n"
+    formatted_str += f"7d Percent Change: {format_decimal(dto['percent_change_7d'])}%\n"
+    formatted_str += f"30d Percent Change: {format_decimal(dto['percent_change_30d'])}%\n"
+    formatted_str += f"60d Percent Change: {format_decimal(dto['percent_change_60d'])}%\n"
+    formatted_str += f"90d Percent Change: {format_decimal(dto['percent_change_90d'])}%\n"
+    formatted_str += f"Market Cap: {format_decimal(dto['market_cap'])}\n"
+    formatted_str += f"Market Cap Dominance: {format_decimal(dto['market_cap_dominance'])}%\n"
+    formatted_str += f"Fully Diluted Market Cap: {format_decimal(dto['fully_diluted_market_cap'])}\n"
+    # formatted_str += f"TVL: {dto['tvl']}\n"
+    formatted_str += f"Last Updated: {format_datetime(dto['last_updated'])}"
 
-async def format_value_data(data: Dict[str, str]) -> str:
-    # Implement your formatting logic here based on the ValueDto class structure
-    # Example formatting: symbol - price - volume_24h
-    formatted_text = f"{data['symbol']} - {data['price']} - {data['volume_24h']}"
-    return formatted_text
+    return formatted_str
+
+def format_decimal(value: str) -> str:
+    if value is not None:
+        try:
+            decimal_value = Decimal(value)
+            return "{:,.2f}".format(decimal_value)
+        except ValueError:
+            return "N/A"
+    else:
+        return "N/A"
+
+def format_datetime(value: str) -> str:
+    if value is not None:
+        try:
+            timestamp = int(value)
+            dt = datetime.fromtimestamp(timestamp)
+            return dt.strftime("%Y-%m-%d %H:%M:%S")
+        except ValueError:
+            return "Invalid Timestamp"
+    else:
+        return "N/A"
